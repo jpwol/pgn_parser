b633146f4a30c740d514afe87a15754311591d0d
jpwol <jpwolfe1981@gmail.com>
2026-03-18 00:42:11 -0400

    chore: updated build to release mode

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
     const exe = b.addExecutable(.{
@@ -9,7 +8,7 @@ pub fn build(b: *std.Build) void {
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
-            .optimize = optimize,
+            .optimize = .ReleaseFast,
         }),
     });
 
