e610035c8a5da99e0630355ab34ce8120c285202
jpwol <jpwolfe1981@gmail.com>
2026-03-29 01:53:39 -0400

    refactor: removed extraneous values from Moves, added support for piece tracking

--- src/globals.zig
+++ src/globals.zig
@@ -2,9 +2,6 @@ pub const Move = struct {
     move_number: u32,
     player: u8,
     move_text: []const u8,
-    is_capture: bool,
-    is_castle: bool,
-    captured_piece: ?u8,
 };
 
 pub const Game = struct {
@@ -13,4 +10,28 @@ pub const Game = struct {
     result: []const u8,
     moves: [1024]Move,
     move_count: usize,
+};
+
+pub const State = struct {
+    white_pawn: u64 = 0x000000000000FF00,
+    white_knight: u64 = 0x0000000000000042,
+    white_bishop: u64 = 0x0000000000000024,
+    white_rook: u64 = 0x0000000000000081,
+    white_queen: u64 = 0x0000000000000010,
+    white_king: u64 = 0x0000000000000008,
+    black_pawn: u64 = 0x00FF000000000000,
+    black_knight: u64 = 0x4200000000000000,
+    black_bishop: u64 = 0x2400000000000000,
+    black_rook: u64 = 0x8100000000000000,
+    black_queen: u64 = 0x1000000000000000,
+    black_king: u64 = 0x0800000000000000,
+};
+
+pub const Piece = enum {
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING,
 };
